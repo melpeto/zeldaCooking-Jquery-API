1998fbaa6734ec6eec048b35eb99e39a2d052fb6
Mel Peto <melanie.peto@gmail.com>
2022-06-12 17:41:34 -0400

    played around with widths of text areas. Added semi-transparent overlay to header to try to improve readability of h1&h2

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// make namespace
 const cookApp = {}
 
-// need variable to access the final array defined in "cookApp.getCookableItemsArray"
-//(It took me forever to make this work so, sorry if this is not the proper way! Feedback welcome :))
 let reducedArray = []
 
-// cache the selector for html elements
 cookApp.$dropdown = $('#userEffectSelection');
 cookApp.$unorderedList = $('.unorderedList');
 cookApp.$listItem = $('.listItem');
@@ -14,8 +10,6 @@ cookApp.$effect = $('.effect');
 cookApp.$image = $('.itemImage');
 cookApp.$description = $('.descriptionText');
 
-// make API call for all items in the game (equipment, treasure, creatures, materials, monsters) 
-// THEN call fn to reduce data to only the arrays of cookable items
 cookApp.getAllItems = () => {
     $.ajax({
         url: 'https://botw-compendium.herokuapp.com/api/v2/all',
@@ -26,14 +20,12 @@ cookApp.getAllItems = () => {
     });
 }
 
-// From all data, get 2 arrays of cookable items (creatures and materials), then combine those into one reduced array to work with
 cookApp.getCookableItemsArray = (wholeObject) => {
     const materialsArray = wholeObject.data.materials;
     const creaturesArray = wholeObject.data.creatures.food;
     reducedArray = [...materialsArray, ...creaturesArray];
 }
 
-//listen for the user's selection from the dropdown 'select' element
 cookApp.userSelection = () => {
     cookApp.$dropdown.on('change', () => {
         cookApp.$unorderedList.empty();
@@ -42,13 +34,11 @@ cookApp.userSelection = () => {
     })
 }
 
-// Filter array of items - get only items with the user's chosen cooking_effect:
 cookApp.filterIt = (array, choice) => {
     const arrayOfItemsWithDesiredEffect = array.filter( (object) => object.cooking_effect === choice);
     cookApp.displayItems(arrayOfItemsWithDesiredEffect);
 }
 
-//Display items on the page, including item name, effect, image and description
 cookApp.displayItems = (itemArray) => {
     itemArray.forEach( (item) => {
         const effect = item.cooking_effect;
@@ -71,13 +61,11 @@ cookApp.displayItems = (itemArray) => {
     })
 }
 
-// Initializer function
 cookApp.init = () => {
     cookApp.getAllItems();
     cookApp.userSelection();
 }
 
-//Document ready function
 $(function() {
     cookApp.init();
 })
